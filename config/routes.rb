732ffa8a661214ae23cda34af7ec275ec3fd4c40
Lily Renard <renard.lily@gmail.com>
2018-10-26 15:43:04 +0200

    add page avec lien vers new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'pages/add'
+  get 'memories/add', to: 'pages#add', as: :add
 
   resources :memories do
     resources :stamps, only: [:new, :create, :edit, :update, :destroy]
   end
-
 end
