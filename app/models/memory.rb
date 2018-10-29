b3f2d2d0abba52cb7543f9ec030e23c9d2e6b2ec
Lily Renard <renard.lily@gmail.com>
2018-10-29 15:13:36 +0100

    sans problèmes de pundit

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,7 +1,8 @@
 class Memory < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   belongs_to :user
   has_and_belongs_to_many :stamps
-  mount_uploader :photo, PhotoUploader
 
   validates :memory_type, presence: true
   validates :title, presence: true
