2e4704da8a2e6853663ba7d41d6512e49bf203dd
Guillaume Herlaut <guillaume_1802@hotmail.fr>
2018-10-29 14:24:16 +0100

    test modal3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
