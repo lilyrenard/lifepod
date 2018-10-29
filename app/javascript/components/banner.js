bf4b67d1021f4e5e8df7fff31592179b14dc5a22
Lily Renard <renard.lily@gmail.com>
2018-10-29 18:27:07 +0100

    home page

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["Select the best moments of your life.", "Discover old memories."],
+    typeSpeed: 70,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,9 @@ import "bootstrap";
 
 import { changeSize } from '../components/cards';
 changeSize();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
+
+import { scrollFunction } from '../components/discover';
+scrollFunction();
