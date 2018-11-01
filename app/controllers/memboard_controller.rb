ffc7ad172cbcdb13b910be69b54c835e5099a9a3
Lily Renard <renard.lily@gmail.com>
2018-11-01 18:00:40 +0100

    algo de pertinance fonctionnel

--- app/controllers/memboard_controller.rb
+++ app/controllers/memboard_controller.rb
@@ -2,27 +2,18 @@ class MemboardController < ApplicationController
   def board
     @memories = policy_scope(Memory)
     # je selectionne toutes les combinaisons de 2 stamps
-    stamps = Stamp.ids.combination(2).to_a
+    # je trie mon array d'id de stamps en fonction de leur nombre de memories
 
-    mem = []
-    stamps.each do |paire|
-      # je compte le nombre de memories associés à la paire
-      mem << policy_scope(Memory).joins(:stamps).where(stamps: { id: paire }).distinct.count
-    end
+    @stamps = Stamp.all
+         .pluck(:id)
+         .combination(2)
+         .to_a
+         .sort_by { |stamp| Memory.joins(:stamps).where(stamps: { id: [stamp[0], stamp[1]] }).group(:id).having("count(*) > 1").distinct.count.length }
+         .reverse
+         .first(3)
+         .map { |id| Stamp.find(id) }
 
-    # je récupère l'index de la paire avec le plus de memories
-    i = mem.index { |x| mem.max }
-    id_s1 = stamps[i][0]
-    id_s2 = stamps[i][1]
 
-    # je trouve les stamps associé
-    @stamp1 = Stamp.find(id_s1).title
-    @stamp2 = Stamp.find(id_s2).title
-
-    #array rangée par ordre croissant
-    mem_s = mem.sort
-    #array rangée par ordre croissant avec les anciens index
-    mem_i = mem.map{|e| mem_s.index(e)}
   end
 
   private
