bc7988ba34e3120f142c77d1df633e2dd3a16af3
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2018-11-07 17:49:40 +0100

    instagram done

---
+++ app/services/instagram_connect_service.rb
@@ -0,0 +1,26 @@
+require 'rest-client'
+class InstagramConnectService
+  def initialize(code, redirect)
+    @code = code
+    @url = "https://api.instagram.com/oauth/access_token"
+    @client_secret = ENV['INSTAGRAM_SECRET_KEY']
+    @client_id = ENV['INSTAGRAM_CLIENT_ID']
+    @redirect = ''
+    if Rails.env == "development"
+      @redirect = "http://localhost:3000#{redirect}"
+    else
+      @redirect = "https://lifepod.herokuapp.com#{redirect}"
+    end
+  end
+
+  def call
+    payload = {
+      grant_type: "authorization_code",
+      code: @code,
+      redirect_uri: @redirect,
+      client_id: @client_id,
+      client_secret: @client_secret
+    }
+    RestClient.post(@url, payload)
+  end
+end
